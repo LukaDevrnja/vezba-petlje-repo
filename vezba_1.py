0b54dda6b66bd2c5c7148b3b6ddd3fa0e5bf818c
Djordje Uskokovic <djordje.uskokovic@link.co.rs>
2024-12-10 18:33:19 +0100

    dodat primer

---
+++ vezba_1.py
@@ -0,0 +1,17 @@
+pocetna_pozicija = 0 
+cilj = 50
+trenutna_pozicija = 0
+brzina= 10
+
+
+for x in range(10): 
+    print(trenutna_pozicija)
+    if trenutna_pozicija == cilj:
+        print("stigao na cilj")
+        break
+    elif trenutna_pozicija > cilj:
+        print("prosli ste")
+    elif trenutna_pozicija < cilj:
+        print("niste jos stigli")
+
+    trenutna_pozicija += brzina
